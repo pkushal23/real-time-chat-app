15fbe8d9b928c73d32525265b761a5be6bc41a56
Kushal Reddy <p.kushal1911@gmail.com>
2025-06-26 14:02:43 +0530

    fixed sortedChats

--- src/components/Chatlist.jsx
+++ src/components/Chatlist.jsx
@@ -38,12 +38,15 @@ const Chatlist = ({ setSelectedUser }) => {
 
 
     const sortedChats = useMemo(() => {
-    return [...chats].sort((a, b) => {
-        const aTimestamp = a?.lastMessageTimestamp?.seconds + a?.lastMessageTimestamp?.nanoseconds / 1e9;
-        const bTimestamp = b?.lastMessageTimestamp?.seconds + b?.lastMessageTimestamp?.nanoseconds / 1e9;
+    return [...chats]
+    .filter(chat => chat?.lastMessageTimestamp?.seconds !== undefined && chat?.lastMessageTimestamp?.nanoseconds !== undefined)
+    .sort((a, b) => {
+        const aTimestamp = a.lastMessageTimestamp.seconds + a.lastMessageTimestamp.nanoseconds / 1e9;
+        const bTimestamp = b.lastMessageTimestamp.seconds + b.lastMessageTimestamp.nanoseconds / 1e9;
         return bTimestamp - aTimestamp;
     });
 }, [chats]);
+
 
     const startChat = (user) => {
         setSelectedUser(user);
